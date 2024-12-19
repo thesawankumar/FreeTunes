531565f7e316ffff40833f8dea99e54feda3e4f2
Nikant Yadav <nikantyadav6803@gmail.com>
2024-12-19 22:06:42 +0000

    deleted extra

--- FreeTunes-main/server/test.py
+++
@@ -1,7 +0,0 @@
-from youtubesearchpython import VideosSearch
-
-item_id = input('song name')
-
-videosSearch = VideosSearch(item_id, limit = 2)
-url = videosSearch.result()['result'][0]['title']
-print(url)
